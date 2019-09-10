b133a90f656d105b28ac1d0f2be6cfe8fa3623cc
Sera Geyran <sgeyran@gmail.com>
2019-09-10 23:23:21 +0300

    Create table example

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/srgyrn/golaxy/model"
+	"log"
+	"os"
+)
+
+// init is called prior to main.
+func init() {
+	// Change the device for logging to stdout.
+	log.SetOutput(os.Stdout)
+}
+
+func main() {
+	model.CreateTableMovie()
+}
--- model/db.go
+++ model/db.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 const (
@@ -15,18 +16,45 @@ const (
 	dbname   = "golaxy"
 )
 
-var Db *sql.DB
-
-func main() {
-	dbCredentials := fmt.Sprintf("host=%s port=%d user=%s" +
-		"password=%s dbname=%s sslmode=disable",
+func initDB() *sql.DB {
+	dbCredentials := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	Db, err := sql.Open("postgres", dbCredentials)
+	db, err := sql.Open("postgres", dbCredentials)
 
-	if errors.Is(err, nil) {
-		panic(err)
+	if !errors.Is(err, nil) {
+		log.Panic(err)
 	}
 
-	defer Db.Close()
+	err = db.Ping()
+
+	if !errors.Is(err, nil) {
+		log.Panic(err)
+	}
+
+	fmt.Println("Database connection successful!")
+
+	return db
+}
+
+func createTable(tableName, createScript string) {
+	db := initDB()
+	defer db.Close()
+
+	stmt, err := db.Prepare(createScript)
+	checkError("preparing statement", tableName, err)
+	defer stmt.Close()
+
+	_, err = stmt.Exec()
+	checkError("executing statement", tableName, err)
+
+	log.Println("Table creation end.")
+}
+
+func checkError(stepDescription, tableName string, err error) {
+	if errors.Is(err, nil) {
+		return
+	}
+
+	log.Printf("Table creation failed at %s | table: %s | %s\n", stepDescription, tableName, err.Error())
 }
