a50cb30974b38084780d0d5ffc771c933b0ee27a
Sera Geyran <sgeyran@gmail.com>
2019-09-15 22:47:38 +0300

    Tests for inserting and reading data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/srgyrn/golaxy/model"
 	"log"
 	"os"
@@ -13,5 +14,39 @@ func init() {
 }
 
 func main() {
-	model.CreateTableMovie()
+	//model.BootstrapDatabase()
+
+	movieGw := new(model.MovieGateway)
+	//insertMovie(movieGw)
+	getMovie(movieGw)
+}
+
+func insertMovie(movieGw *model.MovieGateway) {
+
+	movie := model.Movie{
+		Name:     "Batman",
+		Year:     1989,
+		Director: "Tim Burton",
+		Rating:   7.5,
+		Genre:    []string{"action", "adventure"},
+		Cast:     []string{"Michael Keaton", "Jack Nicholson", "Kim Basinger"},
+	}
+
+	_, err := movieGw.Insert(&movie)
+
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	fmt.Print(movie)
+}
+
+func getMovie(movieGw *model.MovieGateway) {
+	movie, err := movieGw.Find(1)
+
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	fmt.Print(movie)
 }
--- model/db.go
+++ model/db.go
@@ -16,7 +16,15 @@ const (
 	dbname   = "golaxy"
 )
 
-func initDB() *sql.DB {
+type TableGateway interface {
+	Find(id int) (*struct{}, error)
+	FindAll() (*struct{}, error)
+	Insert(*struct{}) (*struct{}, error)
+	Update(*struct{}) (int64, error)
+	Delete(*struct{}) (int64, error)
+}
+
+func getConnection() *sql.DB {
 	dbCredentials := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
@@ -37,24 +45,38 @@ func initDB() *sql.DB {
 	return db
 }
 
-func createTable(tableName, createScript string) {
-	db := initDB()
+func BootstrapDatabase() {
+	db := getConnection()
 	defer db.Close()
 
-	stmt, err := db.Prepare(createScript)
-	checkError("preparing statement", tableName, err)
+	createMovieQuery := `
+		DROP TABLE IF EXISTS movies;
+		CREATE TABLE movies (
+			id serial PRIMARY KEY, 
+			name varchar (200) NOT NULL, 
+			year int NULL CHECK (year > 0), 
+			genre text [] NULL, 
+			rating numeric (2), 
+			director varchar (50), 
+			movie_cast text [], 
+			created_at timestamp DEFAULT CURRENT_TIMESTAMP, 
+			UNIQUE (name, year)
+			)`
+
+	stmt, err := db.Prepare(createMovieQuery)
+	checkError("preparing statement", err)
 	defer stmt.Close()
 
 	_, err = stmt.Exec()
-	checkError("executing statement", tableName, err)
+	checkError("executing statement", err)
 
 	log.Println("Table creation end.")
 }
 
-func checkError(stepDescription, tableName string, err error) {
+func checkError(stepDescription string, err error) {
 	if errors.Is(err, nil) {
 		return
 	}
 
-	log.Printf("Table creation failed at %s | table: %s | %s\n", stepDescription, tableName, err.Error())
+	log.Printf("Table creation failed at %s | %s\n", stepDescription, err.Error())
 }
