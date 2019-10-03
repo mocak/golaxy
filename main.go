7e55320518c01507b0a02243a4d5e3e05970cc01
Murat Ocak <murat.ocak@modanisa.com>
2019-10-03 13:24:12 +0300

    WIP

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"database/sql"
-	"github.com/srgyrn/golaxy/storage"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/srgyrn/golaxy/storage"
 )
 
 // init is called prior to main.
@@ -16,15 +18,72 @@ func init() {
 
 var db *sql.DB
 
-type MoviePostHandler struct {}
+type MoviePostHandler struct{}
 
+func (mph MoviePostHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	movieGw := storage.NewMovieGateway(db)
+	movie := storage.Movie{
+		Name:     "Batman",
+		Year:     1989,
+		Director: "Tim Burton",
+		Rating:   7.5,
+		Genre:    []string{"action", "adventure"},
+		Cast:     []string{"Michael Keaton", "Jack Nicholson", "Kim Basinger"},
+	}
 
-type MovieHandler struct {}
+	_, err := movieGw.Insert(&movie)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, movie.Id)
+}
+
+type MovieGetHandler struct{}
+
+func (mph MovieGetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// id, err := strconv.Atoi(r.URL.Path[len("/movies/"):])
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusBadRequest)
+	// 	return
+	// }
+
+	// movieGW := model.NewMovieGateway(db)
+	// movie, err := movieGW.Find(id)
+	// if err != nil {
+	// 	if err == sql.ErrNoRows {
+	// 		http.Error(w, "", http.StatusNotFound)
+	// 		return
+	// 	}
+
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// writer.Header().Set("Content-Type", "application/json")
+	// writer.WriteHeader(200)
+	// if err = json.NewEncoder(writer).Encode(&movie); err != nil {
+	// 	http.Error(writer, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+}
+
+type MovieHandler struct{}
 
 func (mph MovieHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "POST":
+		mph := MoviePostHandler{}
+		return mph.ServeHTTP(w, r)
+	case "GET":
+		mgh := MovieGetHandler{}
+		return mgh.ServeHTTP(w, r)
+	default:
+		http.Error(w, "", http.StatusMethodNotAllowed)
 		return
 	}
 }
@@ -35,9 +94,6 @@ func main() {
 	defer db.Close()
 
 	http.Handle("/movies/", MovieHandler{})
-
-
-
 
 	log.Fatal(http.ListenAndServe(":8090", nil))
 }
