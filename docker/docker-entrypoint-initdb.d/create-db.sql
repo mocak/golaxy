f934184db939ad28a9ec77646405d109b2be7621
Sera Geyran <sgeyran@gmail.com>
2019-10-06 17:22:32 +0300

    fixed column definition

--- docker/docker-entrypoint-initdb.d/create-db.sql
+++ docker/docker-entrypoint-initdb.d/create-db.sql
@@ -5,7 +5,7 @@ CREATE TABLE movies
     name       varchar(200) NOT NULL,
     year       int       DEFAULT 0 CHECK (year > 0),
     genre      text[]       NULL,
-    rating     numeric(2),
+    rating     numeric(4, 2),
     director   varchar(50),
     movie_cast text[],
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
